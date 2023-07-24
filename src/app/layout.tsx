b9df22eb36ccc0507be0ee69f2a10de3114cb5e3
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-07-24 20:31:05 -0300

    feat(seo): change metadata title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Background Removal',
+  title: 'Background Eraser',
 }
 
 export default function RootLayout({
