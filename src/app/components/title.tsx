f15b4d63720b47e1ee7b7a72dd9c90e7448376ae
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-07-24 19:42:25 -0300

    chore: change project name

--- src/app/components/title.tsx
+++ src/app/components/title.tsx
@@ -1,5 +1,5 @@
 export const Title = () => (
   <h1 className='text-3xl font-bold bg-gradient-to-br from-blue-800 to-blue-600 bg-clip-text text-transparent'>
-    Background Remover
+    Background Eraser
   </h1>
 )
