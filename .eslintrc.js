e128fdcadf08f6e6dcd568ec2255f155ab0d5b41
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-07-25 09:11:13 -0300

    chore(eslint): update config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,14 @@
 module.exports = {
-  extends: ["next/core-web-vitals", "prettier"],
-  plugins: ["prettier"],
+  extends: ['next', 'prettier'],
+  plugins: ['prettier'],
   rules: {
-    "prettier/prettier": "error",
-    "@next/next/no-img-element": "off",
-  },
-};
+    'space-before-function-paren': 'off',
+    'react/prop-types': 'off',
+    'no-use-before-define': 'off',
+    'prefer-const': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    '@next/next/no-img-element': 'off',
+    'prettier/prettier': 'error'
+  }
+}
